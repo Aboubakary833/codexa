c99efa7af9c8dda4a3fc50ef9980d1e33a4d0c1f
Aboubakary833 <aboubakarycisse472@gmail.com>
2026-03-07 06:14:23 +0000

    feat: added new remote related structures

--- internal/domain/domain.go
+++ internal/domain/domain.go
@@ -2,6 +2,7 @@ package domain
 
 import (
 	"errors"
+	"strings"
 	"time"
 )
 
@@ -14,6 +15,7 @@ var (
 	ErrRemoteTechNotFound    = errors.New("remote tech category not found")
 	ErrRemoteSnippetNotFound = errors.New("remote snippet not found")
 
+	ErrCachedManifestNotFound   = errors.New("cached manifest file is not found")
 	ErrSnippetContentNotFound   = errors.New("snippet target file don't exists")
 	ErrSnippetContentCantBeRead = errors.New("snippet target file can't be read")
 )
@@ -47,13 +49,55 @@ type RemoteTech struct {
 	Dirname string `json:"dirname"`
 }
 
+// Match check if a given tech category name match the current tech category
+func (rt RemoteTech) Match(name string) bool {
+	if strings.EqualFold(rt.Name, name) {
+		return true
+	}
+
+	for _, alias := range rt.Aliases {
+		if strings.EqualFold(alias.ID, name) || strings.EqualFold(alias.Name, name) {
+			return true
+		}
+	}
+
+	return false
+}
+
 type RemoteSnippet struct {
 	ID       string `json:"id"`
 	Topic    string `json:"topic"`
 	Filename string `json:"filename"`
 }
 
+func (rs RemoteSnippet) Match(topic string) bool {
+	if strings.EqualFold(rs.ID, topic) || strings.EqualFold(rs.Topic, topic) {
+		return true
+	}
+
+	return strings.Contains(strings.ToLower(rs.Filename), topic)
+}
+
 type Manifest struct {
 	Version string       `json:"version"`
 	Techs   []RemoteTech `json:"techs"`
+}
+
+type CachedManifest struct {
+	Manifest
+	UpdatedAt time.Time
+}
+
+// IsTrustWorthy check if the cached manifest has'nt expired and not empty
+func (cm CachedManifest) IsTrustWorthy() bool {
+	expirationTime := cm.UpdatedAt.Add(time.Minute*30)
+	if cm.UpdatedAt.IsZero() || expirationTime.Before(time.Now()) {
+		return false
+	}
+
+	if len(cm.Techs) == 0 {
+		return false
+	}
+
+	return true
 }
