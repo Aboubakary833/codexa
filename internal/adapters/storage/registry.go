b194f67f24195923ef387f98a5c8feb17328f283
Aboubakary833 <aboubakarycisse472@gmail.com>
2026-03-07 06:07:33 +0000

    (refactor & add): Rewrote registry methods and add new ones

--- internal/adapters/storage/registry.go
+++ internal/adapters/storage/registry.go
@@ -2,9 +2,9 @@ package storage
 
 import (
 	"context"
+	"encoding/json"
 	"errors"
 	"os"
-	"path"
 	"path/filepath"
 	"strings"
 
@@ -12,6 +12,7 @@ import (
 )
 
 type registry struct {
+	// the registry dir path
 	Path string
 }
 
@@ -21,28 +22,23 @@ func NewRegistry(path string) *registry {
 	}
 }
 
-// StoreContent store an entry
-func (r *registry) StoreContent(ctx context.Context, path, content string) error {
-
-	if !strings.HasPrefix(path, r.Path) {
-		path = filepath.Join(r.Path, path)
-	}
-
-	file, err := os.Create(path)
-
+// CreateOrUpdateSnippet store a snippet content or update it if the snippet file already exist.
+func (r *registry) CreateOrUpdateSnippet(ctx context.Context, path, content string) error {
+	file, err := r.openFile(path)
+	
 	if err != nil {
 		return err
 	}
-
+	defer file.Close()
+	
 	_, err = file.Write([]byte(content))
 
 	return err
 }
 
-// LoadContent load a given snippet file content from the local registry
-func (r *registry) LoadContent(ctx context.Context, snippet domain.Snippet) (string, error) {
-	filePath := path.Join(r.Path, snippet.Filepath)
-	rawBytes, err := os.ReadFile(filePath)
+// LoadSnippet load a given snippet file content from the local registry.
+func (r *registry) LoadSnippet(ctx context.Context, snippet domain.Snippet) (string, error) {
+	rawBytes, err := r.readFile(snippet.Filepath)
 
 	if err != nil {
 		switch {
@@ -58,6 +54,39 @@ func (r *registry) LoadContent(ctx context.Context, snippet domain.Snippet) (str
 	}
 
 	return string(rawBytes), nil
+}
+
+// GetCachedManifest return a cached version of the Manifest.
+// If no manifest cache file exists, a new one will automatically be created.
+func (r *registry) GetCachedManifest(ctx context.Context) (domain.CachedManifest, error) {
+	rawBytes, err := r.readFile("index.json")
+
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			return domain.CachedManifest{}, domain.ErrCachedManifestNotFound
+		}
+
+		return domain.CachedManifest{}, err
+	}
+	var manifest domain.CachedManifest
+
+	if err = json.Unmarshal(rawBytes, &manifest); err != nil {
+		return domain.CachedManifest{}, domain.ErrCachedManifestNotFound
+	}
+
+	return manifest, nil
+}
+
+
+func (r *registry) CreateOrUpdateCachedManifest(ctx context.Context, manifest domain.CachedManifest) error {
+	file, err := r.openFile("index.json")
+
+	if err != nil {
+		return err
+	}
+
+	encoder := json.NewEncoder(file)
+	return encoder.Encode(manifest)
 }
 
 // Stat check if the registry exists and is a directory.
@@ -79,4 +108,25 @@ func (r *registry) Stat() error {
 	}
 
 	return nil
+}
+
+// readFile is a helper function for reading and returning a file content
+func (r *registry) readFile(path string) ([]byte, error) {
+	filePath := r.getFileFullPath(path)
+	return os.ReadFile(filePath)
+}
+
+// openFile is the registry helper function for opening file with "READ" and "WRITE" permissions.
+// If the given file does'nt exist, it will be automatically be created.
+func (r *registry) openFile(path string) (*os.File, error) {
+	filePath := r.getFileFullPath(path)
+	return os.OpenFile(filePath, os.O_RDWR | os.O_CREATE | os.O_TRUNC, os.ModePerm)
+}
+
+func (r *registry) getFileFullPath(path string) string {
+	if !strings.HasPrefix(path, r.Path) {
+		path = filepath.Join(r.Path, path)
+	}
+
+	return path
 }
